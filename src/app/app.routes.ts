c27e27331194d4c8da8a0d2b4a24fa1cd372cfba
sagarkori143 <sagarkoriup11@gmail.com>
2024-06-21 21:00:36 +0530

    Added routes to Login and Logout page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CampaignsComponent } from './components/campaigns/campaigns.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent }, 
+    { path: 'campaign', component: CampaignsComponent },
+];
