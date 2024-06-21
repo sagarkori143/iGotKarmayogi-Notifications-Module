cc42a9ae764539ea789e354ba610fc306846eab1
sagarkori143 <sagarkoriup11@gmail.com>
2024-06-21 16:04:59 +0530

    Added Campaigns page completely working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CampaignsComponent } from './components/campaigns/campaigns.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent,NavbarComponent],
+  imports: [RouterOutlet,HomeComponent,NavbarComponent,CampaignsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
